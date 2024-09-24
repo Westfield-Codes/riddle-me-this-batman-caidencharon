71e6d5486036f60e086720aa2e95cdbd54fab75b
caidencharon <caiden204899@student-schoolsofwestfield.org>
2024-09-24 13:50:46 +0000

    new code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" || answer == "pealed") alert("Correct!");
-  else alert("Wrong, Batman! Watch the video, and try again.");
+  var answer = prompt("What do you get when you mix lemons with gun powder? ...");
+  if (answer == "lemonade" || answer == "Lemonade") alert("Correct!");
+  else alert("WRONG DUDE! Keep thinking, and try again.");
 }
